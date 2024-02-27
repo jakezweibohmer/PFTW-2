9aeb9ba1644c5acfdc27f2a7535c3956f22e4764
jakezweibohmer <2treesdesign@mchsi.com>
2024-02-26 18:47:23 -0600

    finishing up style on ex7.1

--- week7/ex7-1Llamas/src/App.jsx
+++ week7/ex7-1Llamas/src/App.jsx
@@ -2,22 +2,25 @@ import Camelids from "./components/Camelid.jsx"
 
 function App() {
 const camelidData = [
-  {name: "Llamas", desc: "A llama can carry as much as 200 pounds for 12 hours a day, but are not ridden."},
-  {name: "Alpacas", desc: "Around 6000 years ago the Andean people started to domesticate alpacas."},
+  {name: "Llamas", 
+  desc: "A llama can carry as much as 200 pounds for 12 hours a day, but are not ridden.", 
+  image: "./images/Llamas_small.jpg"},
+  {name: "Alpacas", desc: "Around 6000 years ago the Andean people started to domesticate alpacas.",
+  image: "./images/Alpacas_small.jpg"},
 ]
   return (
   <div>
-    <h2>( Lama glama )</h2>
+    <h2><a href="https://www.thoughtco.com/fun-facts-about-llamas-3880940">( Lama glama )</a></h2>
     <Camelids 
     name={camelidData[0].name} 
     desc={camelidData[0].desc} 
-    hex={camelidData[0].hex} 
+    image={camelidData[0].image} 
     />
-    <h2>( Vicugna pacos )</h2>
+    <h2><a href="https://www.chillavalleyalpacas.co.uk/38-fascinating-facts-about-alpacas/">( Vicugna pacos )</a></h2>
     <Camelids 
     name={camelidData[1].name} 
     desc={camelidData[1].desc} 
-    hex={camelidData[1].hex} 
+    image={camelidData[1].image} 
     />
   </div>
   )
--- week7/ex7-1Llamas/src/components/Camelid.jsx
+++ week7/ex7-1Llamas/src/components/Camelid.jsx
@@ -1,14 +1,11 @@
 import "./Camelid.css";
-function Camelids({name, desc, hex}) {
+function Camelids({name, desc, image}) {
     return (
         <div className="camelidCard">
             <h1>{name}</h1>
             <p>{desc}</p>
-            <div style={{
-                width: "25px",
-                height: "25px",
-                backgroundColor: hex
-            }}></div>
+            <img src={image} alt={name} />
+            <div></div>
         </div>
     );
 
