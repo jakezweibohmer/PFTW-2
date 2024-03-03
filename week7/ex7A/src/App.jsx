09cb21bfc5dfab3a0a0ebfe516af4262edc75c6f
jakezweibohmer <2treesdesign@mchsi.com>
2024-03-03 11:05:21 -0600

    adjustments to exercise 7A based on new demo

--- week7/ex7A/src/App.jsx
+++ week7/ex7A/src/App.jsx
@@ -1,13 +1,15 @@
-import Cards from "./Cards";
+import "./App.css";
+import { CardsRow } from "./CardsRow";
 
 function App() {
-  const cards = [
+  const cardDecks = [
     {
       name: "Notorious BIG",
       manufacturer: "Theory 11",
       released: "2023",
       description:
         "Straight out of Brooklyn, New York - Notorious B.I.G. Playing Cards are a tribute to one of the greatest rappers of all time. Crafted hand in hand with The Notorious B.I.G. Estate, every aspect of the design was meticulously chosen to reflect the life and legacy of the legendary East Coast MC.",
+      image: "./NotoriousBIG.jpg",
     },
 
     {
@@ -16,6 +18,7 @@ function App() {
       released: "2023",
       description:
         "For the first time, Theory11 made a card box that is fully encased with red velvet and embellished with luxurious gold foil - inspired directly by their track “Red Velvet” and the gold chains that André 3000 and Big Boi would wear during the unforgettable Stankonia era.",
+      image: "./Outkast.jpg",
     },
 
     {
@@ -24,6 +27,7 @@ function App() {
       released: "2022",
       description:
         "The box, the back design, and all card faces have been specially crafted to pay tribute to the King of Rock & Roll. From his early days in Tupelo, Mississippi to becoming a worldwide phenomenon, take the stage with Elvis each time the cards are held in your hand and brought out for play.",
+      image: "./Elvis.jpg",
     },
 
     {
@@ -32,6 +36,7 @@ function App() {
       released: "2022",
       description:
         "I must not fear. Premium playing cards by theory11 bringing all of your favorite characters from Dune to life. Set foot on the harsh desert planet Arrakis and protect the spice from falling into the wrong hands! Follow the journey of Paul Atreides and partake in his mission to ultimately save humanity.",
+      image: "./Dune.jpg",
     },
 
     {
@@ -40,28 +45,39 @@ function App() {
       released: "2022",
       description:
         "The Successor represents two years of unwavering effort to create the MOST LUXURIOUS playing cards ever made. Evoking art from 15th century cathedrals, canvases and manuscripts, The Successor depicts the trials and triumphs of a line of royal succession.",
+      image: "./Successor.jpg",
     },
   ];
 
   return (
     <div>
       <h1>Playing Cards</h1>
-      {cards.map((deck) => {
-        return (
-          <div key={deck.name}>
-            <h2>{deck.name}</h2>
-            <ul>
-              <li>
-                <h3>{deck.manufacturer}</h3>
-              </li>
-              <li>
-                <h3>{deck.released}</h3>
-              </li>
-              <li>{deck.description}</li>
-            </ul>
-          </div>
-        );
-      })}
+      <table className="decks-table">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Deck Name</th>
+            <th>Manufacturer</th>
+            <th>Release Year</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cardDecks.map((decks, index) => {
+            return (
+              <CardsRow
+                key={decks}
+                odd={index % 2 === 0}
+                image={decks.image}
+                name={decks.name}
+                manufacturer={decks.manufacturer}
+                released={decks.released}
+                description={decks.description}
+              />
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ week7/ex7A/src/CardsRow.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/prop-types */
+import "./CardsRow.css";
+export function CardsRow({
+  image,
+  name,
+  manufacturer,
+  released,
+  description,
+  odd,
+}) {
+  return (
+    <tr className={odd && "odd"}>
+      <td>
+        <img src={image} alt={name} />
+      </td>
+      <td style={{ fontWeight: 900, color: "red" }}>{name}</td>
+      <td>{manufacturer}</td>
+      <td>{released}</td>
+      <td>{description}</td>
+    </tr>
+  );
+}
