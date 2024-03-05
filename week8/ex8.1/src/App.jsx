992dea6335dd173af0328d3b6b2219b83be000fc
jakezweibohmer <2treesdesign@mchsi.com>
2024-03-04 19:01:33 -0600

    initial push for ex8.1

---
+++ week8/ex8.1/src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [scoreObj, setScoreObj] = useState({ left: 0, right: 0 });
+  function incrementRightScore() {
+    const newScore = {
+      left: scoreObj.left,
+      right: scoreObj.right + 1,
+    };
+    setScoreObj(newScore);
+  }
+
+  return (
+    <>
+      <h1>Solitare Clicky Game</h1>
+      <div>
+        <button
+          onClick={() => {
+            setScoreObj({ ...scoreObj, left: scoreObj.left + 1 });
+          }}
+        >
+          Alpaca
+        </button>
+      </div>
+      <div>
+        <button onClick={incrementRightScore}>Llama</button>
+      </div>
+      <div>
+        <h2>Scores</h2>
+        Left Score {scoreObj.left} | Right Score {scoreObj.right}
+      </div>
+    </>
+  );
+}
+
+export default App;
