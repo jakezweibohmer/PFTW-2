16bfb8f438d47df661c443734cec3302a1af5a42
jakezweibohmer <2treesdesign@mchsi.com>
2024-03-06 17:27:13 -0600

    made changes after watching class demo recording

--- week8/class-demo/src/App.jsx
+++ week8/class-demo/src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import dog from "./Alpacas_small.jpg";
-import cat from "./Llamas_small.jpg";
+import alpaca from "./assets/Alpacas_small.jpg";
+import llama from "./assets/Llamas_small.jpg";
 import Animal from "./Animal";
 
 function App() {
-  const [pet, setPet] = useState(dog);
+  const [pet, setPet] = useState(alpaca);
   function handleClick(evt) {
     evt.preventDefault();
   }
@@ -17,17 +15,21 @@ function App() {
       <div>
         <Animal url={pet} />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Animals</h1>
       <div className="card">
-        <button onClick={handleClick} id="dogLink">
-          dog
+        <button
+          onClick={() => {
+            setPet(alpaca);
+          }}
+        >
+          Alpaca
         </button>
         <button
           onClick={() => {
-            setPet(cat);
+            setPet(llama);
           }}
         >
-          cat
+          llama
         </button>
       </div>
     </>
