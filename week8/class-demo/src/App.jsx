ccae587f13f48dbb1722c0f4fbc84cdd3da4065e
jakezweibohmer <2treesdesign@mchsi.com>
2024-03-05 19:46:25 -0600

    finished with class demo. Will try to catch up with recording later.

--- week8/class-demo/src/App.jsx
+++ week8/class-demo/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import dog from "./assets/dog.jpg";
-import cat from "./assets/cat.webp";
+import dog from "./Alpacas_small.jpg";
+import cat from "./Llamas_small.jpg";
 import Animal from "./Animal";
 
 function App() {
   const [pet, setPet] = useState(dog);
+  function handleClick(evt) {
+    evt.preventDefault();
+  }
 
   return (
     <>
@@ -16,15 +19,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button>dog</button>
-        <button>cat</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={handleClick} id="dogLink">
+          dog
+        </button>
+        <button
+          onClick={() => {
+            setPet(cat);
+          }}
+        >
+          cat
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
