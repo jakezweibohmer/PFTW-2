bade764ac70415eb2fac2875bdf3feeadb0f161d
jakezweibohmer <2treesdesign@mchsi.com>
2024-03-19 18:59:11 -0500

    finished up demos

--- week9/ex9.1/src/App.jsx
+++ week9/ex9.1/src/App.jsx
@@ -2,35 +2,57 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  // const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
-  // const [passwrd, setPasswrd] = useState("");
-  const [textInputs, setTextInputs] = useState({
+  const [formObject, setFormObject] = useState({
     firstName: "",
     lastName: "",
     passwrd: "",
+    soda: ["pepsi"],
+    pets: true,
+    colors: [],
+    selectResponse: "stompy",
   });
-  function handleTextChange(evt) {
-    console.log(evt.target.value);
-    console.log(evt.target.name);
-    if (evt.target.name === "pets") {
-      setPets((previousPets) => {
-        return !previousPets;
-      });
-    } else {
-      setTextInputs((previousTextInputs) => {
+
+  function handleChange(event) {
+    const simpleValueSetFormItems = [
+      "playerType",
+      "firstName",
+      "lastName",
+      "passwrd",
+    ];
+    if (simpleValueSetFormItems.includes(event.target.name)) {
+      setFormObject((prevForm) => {
         return {
-          ...previousTextInputs,
-          [evt.target.name]: evt.target.value,
+          ...prevForm,
+          [event.target.name]: event.target.value,
         };
       });
     }
+    if (event.target.name === "soda") {
+      setFormObject({
+        ...formObject,
+        soda: event.target.value,
+      });
+    }
+    if (event.target.name === "colors") {
+      if (formObject.colors.includes(event.target.id)) {
+        setFormObject({
+          ...formObject,
+          colors: formObject.colors.filter((color) => {
+            return color !== event.target.id;
+          }),
+        });
+      } else {
+        setFormObject({
+          ...formObject,
+          colors: [...formObject.colors, event.target.id],
+        });
+      }
+    }
   }
-  const [pets, setPets] = useState(true);
+
   function handleSubmit(evt) {
     evt.preventDefault();
-    console.log({ ...textInputs, pets });
-    // console.log({ firstName, lastName, passwrd, pets });
+    console.log("whole form", formObject);
   }
 
   return (
@@ -45,8 +67,8 @@ function App() {
               id="firstName"
               type="text"
               name="firstName"
-              value={textInputs.firstName}
-              onChange={handleTextChange}
+              value={formObject.firstName}
+              onChange={handleChange}
             />
           </div>
           <div>
@@ -55,8 +77,8 @@ function App() {
               id="lastName"
               type="text"
               name="lastName"
-              value={textInputs.lastName}
-              onChange={handleTextChange}
+              value={formObject.lastName}
+              onChange={handleChange}
             />
           </div>
           <div>
@@ -65,8 +87,8 @@ function App() {
               id="passwrd"
               type="password"
               name="passwrd"
-              value={textInputs.passwrd}
-              onChange={handleTextChange}
+              value={formObject.passwrd}
+              onChange={handleChange}
             />
           </div>
           <div>
@@ -75,9 +97,9 @@ function App() {
               id="likesPets"
               type="checkbox"
               name="likesPets"
-              checked={pets}
-              value={pets}
-              onChange={handleTextChange}
+              checked={formObject.pets}
+              value={formObject.pets}
+              onChange={handleChange}
             />
           </div>
         </fieldset>
@@ -86,48 +108,101 @@ function App() {
           <div>
             <label htmlFor="pepsi">
               Pepsi
-              <input id="pepsi" type="radio" name="soda" />
+              <input
+                id="pepsi"
+                type="radio"
+                name="soda"
+                value="pepsi"
+                onChange={handleChange}
+                checked={formObject.soda === "pepsi"}
+              />
             </label>
           </div>
           <div>
             <label htmlFor="coke">
               Coke
-              <input id="coke" type="radio" name="soda" />
+              <input
+                id="coke"
+                type="radio"
+                name="soda"
+                value="coke"
+                onChange={handleChange}
+                checked={formObject.soda === "coke"}
+              />
             </label>
           </div>
           <div>
             <label htmlFor="mtnDew">
               Mountain Dew
-              <input id="mtnDew" type="radio" name="soda" />
+              <input
+                id="mtnDew"
+                type="radio"
+                name="soda"
+                value="mtnDew"
+                onChange={handleChange}
+                checked={formObject.soda === "mtnDew"}
+              />
             </label>
           </div>
         </fieldset>
         <fieldset>
           <legend>Checkbox</legend>
           <label htmlFor="red">
-            {" "}
             Red
-            <input type="checkbox" name="colors" id="red" />
+            <input
+              type="checkbox"
+              name="colors"
+              id="red"
+              value={formObject.colors.includes("red")}
+              onChange={handleChange}
+            />
           </label>
           <label htmlFor="blue">
-            {" "}
             Blue
-            <input type="checkbox" name="colors" id="blue" />
+            <input
+              type="checkbox"
+              name="colors"
+              id="blue"
+              value={formObject.colors.includes("blue")}
+              onChange={handleChange}
+            />
           </label>
           <label htmlFor="green">
-            {" "}
             Green
-            <input type="checkbox" name="colors" id="green" />
+            <input
+              type="checkbox"
+              name="colors"
+              id="green"
+              value={formObject.colors.includes("green")}
+              onChange={handleChange}
+            />
           </label>
           <label htmlFor="cyan">
-            {" "}
             Cyan
-            <input type="checkbox" name="colors" id="cyan" />
+            <input
+              type="checkbox"
+              name="colors"
+              id="cyan"
+              value={formObject.colors.includes("cyan")}
+              onChange={handleChange}
+            />
           </label>
         </fieldset>
         <fieldset>
           <legend>Select</legend>
-          <select name="playerType" id="playerType">
+          <select
+            name="playerType"
+            id="playerType"
+            onChange={handleChange}
+            value={formObject.selectResponse}
+          >
+            {/* {selectOptions.map((option, index) => {
+              return (
+                <option key={index} value={option.toLowerCase()}>
+                  {option}
+                </option>
+              );
+            })} */}
             <option value="aggro">Aggro</option>
             <option value="control">Control</option>
             <option value="stompy">Stompy</option>
