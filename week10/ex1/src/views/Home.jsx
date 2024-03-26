9fb79057e09e5bb0401bdfd70b958bad6743a064
jakezweibohmer <2treesdesign@mchsi.com>
2024-03-26 16:30:46 -0500

    finished adjusting CSS

--- week10/ex1/src/views/Home.jsx
+++ week10/ex1/src/views/Home.jsx
@@ -7,12 +7,15 @@ export function Home() {
       <img src={llamaPic} alt="Llamas" />
       <img src={alpacaPic} alt="Alpacas" />
 
-      <h1>Which are better, llamas or alpacas?</h1>
+      <h1>
+        Which are better, <br />
+        Llamas or Alpacas?
+      </h1>
       <p>
         Is this the last word in the debate? Have you made a choice? Use the
         navigation to learn more about each one and then you can decide which
         win:
-        <strong>Llamas</strong> or <strong>Alpacas</strong>
+        <strong> Llamas</strong> or <strong>Alpacas</strong>
       </p>
     </>
   );
