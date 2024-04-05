d65afbccd3fb8aaca0dc682c7c9ff73f9982fc78
jakezweibohmer <2treesdesign@mchsi.com>
2024-04-04 20:28:59 -0500

    made changes to colors

--- week11/11.A_LightSource/light.js
+++ week11/11.A_LightSource/light.js
@@ -7,11 +7,9 @@ function draw() {
   //move your mouse to change light direction
   let dirX = (mouseX / width - 0.5) * 2;
   let dirY = (mouseY / height - 0.5) * 2;
-  directionalLight(0, 255, 251, -dirX, -dirY, -0.25);
-  fill(255);
+  directionalLight(255, 195, 0, -dirX, -dirY, -0.25);
   noStroke();
-  sphere(200);
-  box(100);
+  sphere(250);
 }
 
 function windowResized() {
