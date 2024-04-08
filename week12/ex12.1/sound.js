3a78a8dcad0ad50f6771b4cf7ff25ade5e04cc75
jakezweibohmer <2treesdesign@mchsi.com>
2024-04-08 18:15:43 -0500

    loaded a new mp3 and adjust window size

--- week12/ex12.1/sound.js
+++ week12/ex12.1/sound.js
@@ -3,8 +3,9 @@ var button;
 var amp;
 
 function setup() {
-  createCanvas(800, 400);
-  song = loadSound("rainbow.mp3", loaded);
+  createCanvas(windowWidth, windowHeight);
+  // Corrosion of Conformity's "The Door"
+  song = loadSound("TheDoor.mp3", loaded);
   amp = new p5.Amplitude();
   background(51);
 }
@@ -15,11 +16,11 @@ function loaded() {
 }
 
 function draw() {
-  background(51);
+  background(255);
 
   var vol = amp.getLevel();
-  var diam = map(vol, 0, 0.3, 20, 500);
-
+  var diam = map(vol, 0, 0.3, 30, 1200);
+  noStroke();
   fill(255, 0, 255);
   ellipse(width / 2, height / 2, diam, diam);
 }
@@ -33,4 +34,9 @@ function togglePlaying() {
     song.stop();
     button.html("play");
   }
+}
+
+function windowResized() {
+  // Resize canvas when window size changes
+  resizeCanvas(windowWidth, windowHeight);
 }
