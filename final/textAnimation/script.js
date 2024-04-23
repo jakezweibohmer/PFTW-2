c4e6ebe510a76859e9135fe36a325812fef4e5eb
jakezweibohmer <2treesdesign@mchsi.com>
2024-04-23 11:38:22 -0500

    initial push for particle text

---
+++ final/textAnimation/script.js
@@ -0,0 +1,65 @@
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let particleArray = [];
+
+// handle mouse
+const mouse = {
+  x: null,
+  y: null,
+  radius: 150,
+};
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  //   console.log(mouse.x, mouse.y);
+});
+
+ctx.fillStyle = "white";
+ctx.font = "30px Verdana";
+ctx.fillText("A", 0, 40);
+const data = ctx.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    // radius of particles
+    this.size = 3;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    // how heavy particles are and how fast they move away from mouse
+    this.density = Math.random() * 30 + 1;
+  }
+
+  draw() {
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+  }
+}
+
+function init() {
+  particleArray = [];
+  for (let i = 0; i < particleArray.length; i++) {
+    let x = Math.random() * 500;
+    let y = Math.random() * 500;
+    particleArray.push(new Particle(x, y));
+  }
+  //   particleArray.push(new Particle(50, 50));
+}
+init();
+// console.log(particleArray);
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].draw();
+  }
+  requestAnimationFrame(animate);
+}
+animate();
