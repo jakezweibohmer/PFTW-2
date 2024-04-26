100d9e8f8e3c35c751e64fad2add238809d1c84b
jakezweibohmer <2treesdesign@mchsi.com>
2024-04-26 15:42:49 -0500

    finished initial programming. Works great!

--- final/particleText/script.js
+++ final/particleText/script.js
@@ -2,16 +2,55 @@
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext("2d", {
+    willReadFrequently: true,
+  });
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
   class Particle {
-    constructor() {}
+    constructor(effect, x, y, color) {
+      this.effect = effect;
+      this.x = Math.random() * this.effect.canvasWidth;
+      this.y = Math.random() * this.effect.canvasHeight;
+      this.color = color;
+      this.originX = x;
+      this.originY = y;
+      this.size = this.effect.gap - 1;
+      this.dx = 0;
+      this.dy = 0;
+      this.vx = 0;
+      this.vy = 0;
+      this.force = 0;
+      this.angle = 0;
+      this.distance = 0;
+      // how particles interact with mouse movements
+      this.friction = Math.random() * 0.6 + 0.15;
+      this.ease = Math.random() * 0.1 + 0.005;
+    }
 
-    draw() {}
+    draw() {
+      this.effect.context.fillStyle = this.color;
+      this.effect.context.fillRect(this.x, this.y, this.size, this.size);
+    }
 
-    update() {}
+    update() {
+      this.dx = this.effect.mouse.x - this.x;
+      this.dy = this.effect.mouse.y - this.y;
+      this.distance = this.dx * this.dx + this.dy * this.dy;
+      this.force = -this.effect.mouse.radius / this.distance;
+
+      if (this.distance < this.effect.mouse.radius) {
+        this.angle = Math.atan2(this.dy, this.dx);
+        this.vx += this.force * Math.cos(this.angle);
+        this.vy += this.force * Math.sin(this.angle);
+      }
+
+      this.x +=
+        (this.vx *= this.friction) + (this.originX - this.x) * this.ease;
+      this.y +=
+        (this.vy *= this.friction) + (this.originY - this.y) * this.ease;
+    }
   }
 
   class Effect {
@@ -21,7 +60,7 @@ window.addEventListener("load", function () {
       this.canvasHeight = canvasHeight;
       this.textX = this.canvasWidth / 2;
       this.textY = this.canvasHeight / 2;
-      this.fontSize = 200;
+      this.fontSize = 150;
       this.lineHeight = this.fontSize * 0.8;
       this.maxTextWidth = this.canvasWidth * 0.8;
       this.textInput = document.getElementById("textInput");
@@ -30,6 +69,18 @@ window.addEventListener("load", function () {
           this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
           this.wrapText(e.target.value);
         }
+      });
+      // particle text
+      this.particles = [];
+      this.gap = 3;
+      this.mouse = {
+        radius: 20000,
+        x: 0,
+        y: 0,
+      };
+      window.addEventListener("mousemove", (e) => {
+        this.mouse.x = e.x;
+        this.mouse.y = e.y;
       });
     }
     wrapText(text) {
@@ -48,7 +99,8 @@ window.addEventListener("load", function () {
       this.context.textBaseline = "middle";
       this.context.lineWidth = 3;
       this.context.strokeStyle = "white";
-      this.context.font = this.fontSize + "px Helvetica";
+      // change font to what you linked to Google fonts on index.html
+      this.context.font = this.fontSize + "px Kavoon";
 
       // break multiline text
       let linesArray = [];
@@ -79,36 +131,61 @@ window.addEventListener("load", function () {
           this.textY + index * this.lineHeight
         );
       });
+      this.convertToParticles();
+    }
+    convertToParticles() {
+      this.particles = [];
+      const pixels = this.context.getImageData(
+        0,
+        0,
+        this.canvasWidth,
+        this.canvasHeight
+      ).data;
+      this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+      for (let y = 0; y < this.canvasHeight; y += this.gap) {
+        for (let x = 0; x < this.canvasWidth; x += this.gap) {
+          const index = (y * this.canvasWidth + x) * 4;
+          const alpha = pixels[index + 3];
+          if (alpha > 0) {
+            const red = pixels[index];
+            const green = pixels[index + 1];
+            const blue = pixels[index + 2];
+            const color = "rgb(" + red + ", " + green + ", " + blue + ")";
+            this.particles.push(new Particle(this, x, y, color));
+          }
+        }
+      }
     }
 
-    convertToParticles() {}
-
-    render() {}
+    render() {
+      this.particles.forEach((particle) => {
+        particle.update();
+        particle.draw();
+      });
+    }
+    resize(width, height) {
+      this.canvas.canvasWidth = width;
+      this.canvas.canvasHeight = height;
+      this.textX = this.canvasWidth / 2;
+      this.textY = this.canvasHeight / 2;
+      this.maxTextWidth = this.canvasWidth * 0.8;
+    }
   }
   const effect = new Effect(ctx, canvas.width, canvas.height);
-  effect.wrapText("Hello");
+  effect.wrapText(effect.textInput.placeholder);
+  effect.render();
 
-  function animate() {}
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    effect.render();
+    requestAnimationFrame(animate);
+  }
+  animate();
 
-  //   function wrapText(text) {
-  //     let linesArray = [];
-  //     let lineCounter = 0;
-  //     let line = "";
-  //     let words = text.split(" ");
-  //     for (let i = 0; i < words.length; i++) {
-  //       let testLine = line + words[i] + " ";
-  //       if (ctx.measureText(testLine).width > maxTextWidth) {
-  //         line = words[i] + " ";
-  //         lineCounter++;
-  //       } else {
-  //         line = testLine;
-  //       }
-  //       linesArray[lineCounter] = line;
-  //     }
-  //     let textheight = lineHeight * lineCounter;
-  //     let textY = canvas.height / 2 - lineHeight / 2;
-  //     linesArray.forEach((el, index) => {
-  //       ctx.fillText(el, canvas.width / 2, textY + index * lineHeight);
-  //     });
-  //   }
+  window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width, canvas.height);
+    effect.wrapText(effect.textInput.placeholder);
+  });
 });
